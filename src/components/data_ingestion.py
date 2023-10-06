c4157bf58dd5e3ceb19730dd6659365573c27bb6
shivam_uppal <shivsen1997@gmail.com>
2023-10-06 15:51:10 +0530

    updated data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,49 +7,43 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from src.components.data_transformation import DataTransformation, DataTransformationConfig
-
 @dataclass
-class Dataingestionconfig:
-    train_data_path : str = os.path.join("artifacts", "train.csv")
-    test_data_path : str = os.path.join("artifacts", "test.csv")
-    raw_data_path : str = os.path.join("artifacts", "raw_data.csv")
-
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config = Dataingestionconfig()
-    
-    def initiate_data_ingestion (self):
-        logging.info("Enterted in Data Ingestion")
+        self.ingestion_config=DataIngestionConfig()
 
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv("notebook\data\stud.csv")
+            df=pd.read_csv('notebook\data\stud.csv')
+            logging.info('Read the dataset as dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
-            
-            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
-            logging.info("Initiating Train Test Split")
-            train_set, test_set = train_test_split(df, train_size=0.8, random_state=42)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
-            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header = True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header = True)
+            logging.info("Train test split initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
-            logging.info("Ingestion of the Data is completed")
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
 
-            return (
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("Inmgestion of the data iss completed")
+
+            return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
-            )
 
+            )
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CustomException(e,sys)
         
 if __name__=="__main__":
-    obj = DataIngestion()
-    train_data, test_data = obj.initiate_data_ingestion()
-
-
-    data_transformation = DataTransformation() 
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
--- src/logger.py
+++ src/logger.py
@@ -2,19 +2,16 @@ import logging
 import os
 from datetime import datetime
 
-Log_file = f"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log"
-Log_path = os.path.join(os.getcwd(), "logs", Log_file)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-os.makedirs( Log_path, exist_ok=True)
-
-Log_file_path = os.path.join(Log_path, Log_file)
-
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
-    filename=Log_file_path,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
-)
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 
-if __name__=="__main__":
-    logging.info("logging has started")
+
+)
